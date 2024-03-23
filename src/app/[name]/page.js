126d80ce932fc36e544775e6d252b50dc877c466
patrik moravec <patri.moraevc@email.cz>
2024-03-23 16:22:26 +0100

    odstraneni zbytečného filu

--- src/app/[name]/page.js
+++ src/app/[name]/page.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import styles from './country/page.module.css';
+import styles from './page.module.css';
 
 export default async function CountryDetails({ params }) {
     const response = await fetch(`https://restcountries.com/v3.1/name/${params.name}`);
@@ -10,7 +10,6 @@ export default async function CountryDetails({ params }) {
     const flagDesc = data[0].flags.alt;
     const area = data[0].area;
     const population = data[0].population;
-
 
     return (
         <main>
